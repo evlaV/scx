4ba42914bd1320291f8b2c64fad98fe9d0e7d72e
Changwoo Min <changwoo@igalia.com>
2025-09-08 10:16:53 +0900

    build: Vendorizing Rust crates for offline build.
    Signed-off-by: Changwoo Min <changwoo@igalia.com>

---
+++ vendor/sscanf/tests/fail/missing_params.rs
@@ -0,0 +1,8 @@
+fn main() {
+    sscanf::sscanf!();
+    sscanf::sscanf!("5");
+    sscanf::sscanf!("5",);
+    sscanf::sscanf!("5", 'x');
+    sscanf::sscanf!("5", usize);
+    sscanf::sscanf_get_regex!();
+}
